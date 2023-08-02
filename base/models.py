2f620e4a25563154ccd12224fbee787d2c1821ea
Namakula Josephine <jnamakula324@gmail.com>
2023-08-02 16:37:27 +0300

    todo_list_app

---
+++ base/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    due = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True,null=True)
+    time =models.TimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
+
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering =['complete']
---
+++ base/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+
+from . views import CustomLoginView, TaskList,TaskDetail,TaskCreate,TaskUpdate,DeleteView,RegisterPage, PendingTaskList, FinishedTaskList, SkippedTaskList
+from django.contrib.auth.views import LogoutView
+
+
+urlpatterns = [
+    path('accounts/login/', CustomLoginView.as_view (),name='login'),
+    path('logout/', LogoutView.as_view (next_page='login'),name='logout'),
+    path('register/',RegisterPage.as_view(), name='register'),
+    path('',TaskList.as_view(),name='tasks'),
+    path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
+    path('create-task/',TaskCreate.as_view(),name='task-create'),
+    path('task-update/<int:pk>/',TaskUpdate.as_view(),name='task-update'),
+    path('task-delete/<int:pk>/',DeleteView.as_view(),name='task-delete'),
+    path('pending/', PendingTaskList.as_view(), name='pending_tasks'),
+     path('finished/', FinishedTaskList.as_view(), name='finished_tasks'),
+      path('skipped/', SkippedTaskList.as_view(), name='skipped_tasks'),
+     path('task-delete/<int:pk>/',DeleteView.as_view(),name='task-delete'),
+     path('calendar/', views.CalendarView.as_view(), name='calendar'),
+]
